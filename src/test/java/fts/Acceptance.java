d4a94cc32b7f770187b877800fa9f9a15d502853
alimohsen14 <Ali.mohsen2003@outlook.com>
2024-12-09 13:27:48 +0200

    Initial commit

---
+++ src/test/java/fts/Acceptance.java
@@ -0,0 +1,15 @@
+package fts;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "usecase",
+        plugin = {"summary","html:target/cucumber/report.html"},
+        monochrome = true,
+        snippets = CucumberOptions.SnippetType.CAMELCASE ,
+        glue = "fts")
+public class Acceptance {
+}
---
+++ src/test/java/fts/logingSteps.java
@@ -0,0 +1,38 @@
+package fts;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class logingSteps {
+    @Given("the user is on the login page")
+    public void theUserIsOnTheLoginPage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user enters valid credentials")
+    public void theUserEntersValidCredentials() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the user should be redirected to the dashboard")
+    public void theUserShouldBeRedirectedToTheDashboard() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
+    @When("the user enters invalid credentials")
+    public void theUserEntersInvalidCredentials() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("an error message should be displayed")
+    public void anErrorMessageShouldBeDisplayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
