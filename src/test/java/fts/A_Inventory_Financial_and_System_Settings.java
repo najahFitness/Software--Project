68a7d5c59f4a9339f743863156f8fa0da04877c1
Nimer-Asaad <nimerziad42@gmail.com>
2024-12-14 16:19:18 +0200

    final update feature

---
+++ src/test/java/fts/A_Inventory_Financial_and_System_Settings.java
@@ -0,0 +1,51 @@
+package fts;
+
+public class A_Inventory_Financial_and_System_Settings {
+
+
+    @Given("Admin logs in to the system")
+    public void adminLogsInToTheSystem() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Admin navigates to the Inventory Management section")
+    public void adminNavigatesToTheInventoryManagementSection() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Admin receives information from instructors on protein shortages or equipment needs")
+    public void adminReceivesInformationFromInstructorsOnProteinShortagesOrEquipmentNeeds() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Admin can order replacements or new equipment")
+    public void adminCanOrderReplacementsOrNewEquipment() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    @When("Admin navigates to the Financial Management section")
+    public void adminNavigatesToTheFinancialManagementSection() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Admin can view and manage financial data, including subscription payments and outstanding fees")
+    public void adminCanViewAndManageFinancialDataIncludingSubscriptionPaymentsAndOutstandingFees() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    @When("Admin navigates to the System Settings section")
+    public void adminNavigatesToTheSystemSettingsSection() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Admin can configure system preferences, notifications, and other settings")
+    public void adminCanConfigureSystemPreferencesNotificationsAndOtherSettings() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
