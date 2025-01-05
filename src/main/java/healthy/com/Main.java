570ace2bf4be7dcd5115d4e241fe9fc7d9456e00
alimohsen14 <Ali.mohsen2003@outlook.com>
2025-01-05 21:11:36 +0200

    first work in bad smil1

--- src/main/java/healthy/com/Main.java
+++ src/main/java/healthy/com/Main.java
@@ -1,7 +1,7 @@
 package healthy.com;
 
 import java.util.Scanner;
-//ali11
+//ali111
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
